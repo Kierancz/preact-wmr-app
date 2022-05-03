e478da7311fc34fd1bc1a0ab2f80ca3b809e2a61
Kieran Czerwinski <kierwinski@gmail.com>
2022-05-02 22:59:00 -0600

    ci: add basic ghactions CI

--- public/pages/home/index.spec.tsx
+++ public/pages/home/index.spec.tsx
@@ -1,7 +1,7 @@
 import { h } from "preact";
 import { render } from "@testing-library/preact";
 
-import Home from ".";
+import Home from "./index";
 
 test("shows the children when the checkbox is checked", () => {
   const { getByText } = render(<Home />);
